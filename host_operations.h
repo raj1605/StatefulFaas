a4c495d1f6dc66f4bf6aaf139213384ca4d07d0a
raj1605 <monishraj@live.com>
2023-05-02 00:12:46 -0400

    Router req model 3

--- host_operations.h
+++ host_operations.h
@@ -39,7 +39,9 @@ own wasm_trap_t *get(void* env, const wasm_val_vec_t* args, wasm_val_vec_t* resu
 own wasm_trap_t *put(void* env, const wasm_val_vec_t* args, wasm_val_vec_t* results){
 
     arr[args->data[0].of.i32] = arr[args->data[1].of.i32];
-
+    for(int i = 0;i<arr.size;i++)
+        printf("%d\n", arr[i]);
+    printf("\n");
     return NULL;
 }
 

57e509078c3686eb0c0bf15d3da80ce416a3c209
raj1605 <monishraj@live.com>
2023-05-02 09:49:56 -0400

    Router req model 5

--- fib_test.cpp
+++ fib_test.cpp
@@ -35,7 +35,7 @@ void run_function(socket_t* chainResponse, std::string *key_temp, char func_id, 
 //    register_fun();
 
     // Initialize.
-    message_t key(*key_temp, strlen(*key_temp));
+    message_t key(*key_temp, key_temp->length());
     message_t discard("", 0);
     std::cout << std::endl;
     printf("\nInitializing...\n");

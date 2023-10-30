2b881c7e366732301e728b79e40e20e9f0cb7c79
raj1605 <monishraj@live.com>
2023-10-30 06:53:06 +0000

    2

---
+++ interaction_core.h
@@ -0,0 +1,4 @@
+extern "C" void chain_call(int, const char*) __attribute__((__import_module__("env"), __import_name__("chain_call")));
+extern "C" int get(int) __attribute__((__import_module__("env"), __import_name__("get")));
+extern "C" void put(int,int,int) __attribute__((__import_module__("env"), __import_name__("put")));
+extern "C" void use_future(int) __attribute__((__import_module__("env"), __import_name__("use_future")));
